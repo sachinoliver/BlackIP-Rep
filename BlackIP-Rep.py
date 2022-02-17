0feb7d5c0ab93fde1ac151c13298e7b8a4346732
JudePaul <63412160+Jude-Paul@users.noreply.github.com>
2022-02-17 17:08:12 +0530

    Added optional virustotal functionality and more
    Improved optional arguments and better structure added.

--- BlackIP-Rep.py
+++ BlackIP-Rep.py
@@ -1,8 +1,10 @@
 import re, csv, glob, sys
-import asyncio, os
-import pycountry_convert as pc
+import asyncio, os, argparse
 from collections import defaultdict
+import pycountry_convert as pc
 from rich import print
+from rich.console import Console
+from rich.style import Style
 from rich.table import Column, Table
 from selenium import webdriver
 from pyppeteer import launch
@@ -10,27 +12,29 @@ from selenium.webdriver.firefox.options import Options
 from time import sleep
 from bs4 import BeautifulSoup
 
+console = Console()
+style = Style()
 
 def Banner():
-    print("""
+    console.print("""[#00FFFF]
 ██████╗ ██╗      █████╗  ██████╗██╗  ██╗██╗██████╗       ██████╗ ███████╗██████╗
 ██╔══██╗██║     ██╔══██╗██╔════╝██║ ██╔╝██║██╔══██╗      ██╔══██╗██╔════╝██╔══██╗
 ██████╔╝██║     ███████║██║     █████╔╝ ██║██████╔╝█████╗██████╔╝█████╗  ██████╔╝
 ██╔══██╗██║     ██╔══██║██║     ██╔═██╗ ██║██╔═══╝ ╚════╝██╔══██╗██╔══╝  ██╔═══╝
 ██████╔╝███████╗██║  ██║╚██████╗██║  ██╗██║██║           ██║  ██║███████╗██║
 ╚═════╝ ╚══════╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝           ╚═╝  ╚═╝╚══════╝╚═╝
-                                                                                 """)
+                                                                                 """,style="")
 
 
 def arguments():
     parser = argparse.ArgumentParser(usage="python3 "+sys.argv[0]+" file_contains_ip_list.txt")
     parser.add_argument("file",help="A file containing ip address should be provided")
-    parser.add_argument("-vt", '--virustotal' ,help="Screenshot from Virus Total")
-    # parser.add_argument("-v", "--verbosity", type=int,
-    #                     help="increase output verbosity",default=0)
+    parser.add_argument("-vt", "--virustotal",help="Takes screenshots for evidence with virustotal",action='store_true')
+    # parser.add_argument("-v", "--verbosity", type=int,help="increase output verbosity",default=0)
     args = parser.parse_args()
-    t = "test"
-    return args.file, t
+    vt = args.virustotal
+
+    return args.file, vt
 
 def directorys():
     try:
@@ -39,8 +43,8 @@ def directorys():
         pass
 
 async def spam():
-    print("[+] Starting...")
-    file, t = arguments()
+    print("[#00FF00][+] Starting...")
+    file, vt = arguments()
     with open(file, 'r') as f:
         lines = f.readlines()
     browser = await launch(options={'args': ['--no-sandbox']}, headless=True)
@@ -63,8 +67,9 @@ async def spam():
     for i in range(len(list1)):
         l = str(list1[i])
         if "r.png" in l:
-            ip = re.findall(r'[0-9]+(?:.[0-9]+){3}', l )
+            ip = re.findall(r'[0-9]+(?:.[0-9]+){3}', l)
             ip = ip[0]
+            
             with open('unsorted.txt', 'a') as f:
                 f.write('%s\n' %ip)
             f.close()
@@ -82,8 +87,8 @@ async def spam():
         f.close()
     f.close()
 
+def screenshots(value=False):
     print("[+] Taking Screenshots...")
-
     file = open('ip.txt')
     for line in file:
         print("IP: {}".format(line.strip()))
@@ -92,10 +97,12 @@ async def spam():
         option.headless = True
         driver = webdriver.Firefox(options=option)
         driver.set_window_size(1920,1080)
-        driver.get('https://mxtoolbox.com/SuperTool.aspx?action=blacklist%3a' + ips + '&run=toolpage')
-        #driver.get('https://www.virustotal.com/gui/ip-address/' + ip )
+        if value == True:
+            driver.get('https://www.virustotal.com/gui/ip-address/' + ips )
+        elif value == False:
+            driver.get('https://mxtoolbox.com/SuperTool.aspx?action=blacklist%3a' + ips + '&run=toolpage')
+        
         sleep(4)
-
         driver.get_screenshot_as_file("BlackIP-Rep/screenshots/"+ips+".png")
         driver.quit()
     file.close
@@ -107,7 +114,7 @@ async def whois():
     file, t = arguments()
     with open(file, 'r') as f:
         lines = f.readlines()
-    browser = await launch(options={'args': ['--no-sandbox']}, headless=False)
+    browser = await launch(options={'args': ['--no-sandbox']}, headless=True)
     page = await browser.newPage()
     await page.goto("https://www.infobyip.com/ipbulklookup.php/")
     await page.waitForSelector("[name=ips]")
@@ -131,7 +138,7 @@ async def whois():
 
 
 
-def conti():
+def conti(value=True):
     columns = defaultdict(list)
     file = './BlackIP-Rep/whoislookup.csv'
     with open(file) as f:
@@ -146,45 +153,52 @@ def conti():
         city = columns[4]
 
     test = 0
-    for i in tqdm(range(len(regions))):
+    for i in range(len(regions)):
         if regions[test] == "":
             regions[test] = "None"
         if city[test] == "":
             city[test] = "None"
         test += 1
-        sleep(0.02)
+
     table = Table(show_header=True, header_style="bold magenta")
     table.add_column("NO:", style="dim")
     table.add_column("IP", style="dim")
     table.add_column("Continent")
     table.add_column("Country", justify="left")
-    table.add_column("Region", justify="left")
-    table.add_column("City", justify="left")
-
+    table.add_column("Region")
+    table.add_column("City")
+    
 
     qq = 0
     for cont in country:
         if cont != "Private network":
             country_code = pc.country_name_to_country_alpha2(cont , cn_name_format="default")
             continent_name = pc.country_alpha2_to_continent_code(country_code)
-            #print(f"IP: {ip[qq]} Continent: {continent_name}, Country: {cont} ({country_code}), {regions[qq]}, {city[qq]}")
             table.add_row(
     str(qq + 1),"[cyan]"+ip[qq]+"[/cyan]","[yellow]" + continent_name + "[/yellow]", cont + " ("+ country_code +")" , regions[qq], city[qq]
     )
             qq += 1
         else:
-           #print(f"IP: {ip[qq]}  Continent: None , Country: Private network, region: None, City: None")
             table.add_row(
     str(qq + 1),"[red]"+ip[qq]+"[/red]", "[green]"+continent_name+"[/green]", cont, regions[qq], city[qq]
     )
     print(table)
     print("[+] Files has been stored at: {}/BlackIP-Rep".format(os.getcwd()))
 
-Banner()
-arguments()
-directorys()
-asyncio.get_event_loop().run_until_complete(spam())
-asyncio.get_event_loop().run_until_complete(whois())
-conti()
-print("[+] Done")
-print("[+] Happy Hacking")
+def main():
+    Banner()
+    file, vt = arguments()
+    arguments()
+    directorys()
+    asyncio.get_event_loop().run_until_complete(spam())
+    if vt == True:
+        screenshots(True)
+    else:
+        screenshots()
+    asyncio.get_event_loop().run_until_complete(whois())
+    conti()
+    print("[+] Done")
+    print("[+] Happy Hacking")
+
+
+main()
